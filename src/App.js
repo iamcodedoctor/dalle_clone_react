b86205885ee13222f30da513ac294ef5db9adae5
iamcodedoctor <steve64725@gmail.com>
2023-03-09 21:06:00 +0530

    basic site structure and home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom'
+import { Home, CreatePost } from './pages'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 ">
+                <Link to="/">
+                    <p className="text-2xl">Open AI</p>
+                    {/* w-28 object-contained */}
+                </Link>
+                <Link
+                    to="/create-post"
+                    className="font-inter font-medium text-white px-4 py-2 rounded-md bg-[#6666EA]"
+                >
+                    Create
+                </Link>
+            </header>
+            <main className="sm:p-8 px-4 py-8 w-full bg-[#F1EFEE] min-h-[calc(100vh-73px)]">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/create-post" element={<CreatePost />} />
+                </Routes>
+            </main>
+        </BrowserRouter>
+    )
 }
 
-export default App;
+export default App
